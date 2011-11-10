3a64309bf96b1e11085d627bb643e75ff815e78b
Régis Guyomarch <regisg@proxiweb.fr>
2011-11-10 14:23:40 +0100

    first version of the extension

---
+++ twitter_bootstrap/templates/project/manifest.rb
@@ -0,0 +1,16 @@
+# Make sure you list all the project template files here in the manifest.
+description "Twitter Boostrap framework"
+stylesheet 'screen.scss', :media => 'screen, projection'
+discover :javascripts
+
+help %Q{
+Please see the twitter bootstrap website for documentation on how twitter boostrap works:
+
+    http://twitter.github.com/bootstrap/
+}
+
+welcome_message %Q{
+Please see the twitter bootstrap website for documentation on how twitter boostrap works:
+
+    http://twitter.github.com/bootstrap/
+}
